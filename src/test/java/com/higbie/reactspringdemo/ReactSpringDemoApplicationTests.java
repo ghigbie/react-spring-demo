184bbb5b13fee86d99d608eefffaa13f21f48a25
ghigbie <gehigbie@gmail.com>
2019-11-10 18:00:28 -0700

    Initial commit

---
+++ src/test/java/com/higbie/reactspringdemo/ReactSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.higbie.reactspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
