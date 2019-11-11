cb728e6d6f76b3cbbb3ff128714cac0f3eb58144
ghigbie <gehigbie@gmail.com>
2019-11-10 18:45:12 -0700

    Add client and unfetch

--- src/main/java/com/higbie/reactspringdemo/student/StudentController.java
+++ src/main/java/com/higbie/reactspringdemo/student/StudentController.java
@@ -14,8 +14,16 @@ public class StudentController {
     @GetMapping
     public List<Student> getAllStudents() {
         return List.of(
-              new Student(UUID.randomUUID(),"James","Bond", "jamesbond@gmail.com", Student.Gender.MALE),
-                new Student(UUID.randomUUID(),"Elisa","Tamara", "elisatamara@gmail.com", Student.Gender.FEMALE)
+                new Student(UUID.randomUUID(),
+                         "James",
+                         "Bond",
+                         "jamesbond@gmail.com",
+                          Student.Gender.MALE),
+                new Student(UUID.randomUUID(),
+                           "Elisa",
+                            "Tamara",
+                           "elisatamara@gmail.com",
+                            Student.Gender.FEMALE)
         );
     }
 }
