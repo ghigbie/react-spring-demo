7f94814b4567ac925415613b4b91bd7a06781308
ghigbie <gehigbie@gmail.com>
2019-11-10 18:06:56 -0700

    Add getters and setters

--- src/main/java/com/higbie/reactspringdemo/student/Student.java
+++ src/main/java/com/higbie/reactspringdemo/student/Student.java
@@ -1,4 +1,5 @@
 package com.higbie.reactspringdemo.student;
+import java.util.UUID;
 
 public class Student {
 
@@ -8,8 +9,34 @@ public class Student {
     private final String email;
     private final Gender gender;
 
+    public Student(String firstName, String lastName, String email, Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+    }
+
     enum Gender {
         MALE, FEMALE
     }
 
+    public UUID getStudentId() {
+        return studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
 }
