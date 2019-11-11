006300697f2ed664ccdd0b158138e40ac2a6f473
ghigbie <gehigbie@gmail.com>
2019-11-10 19:28:30 -0700

    Add fucntional mapping and conditioanl rendering

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import './App.css';
 import { getAllStudents } from './client';
 
 const App = () => {
-  getAllStudents().then(res => res.json().then(students => {
-    console.log(students) 
-  }));
+  const [students, setStudents] = useState([]);
+  const fetchStudents = () => {
+    getAllStudents()
+      .then(res => res.json()
+      .then(students => {
+        console.log(students);
+        setStudents(students);
+      }
+      ));
+  };
+  useEffect(() => {
+    fetchStudents();
+  },[]);
+
+
   return (
     <div className="App">
+
+      {students ? 
+        students.map(student => (<div key={student.studentId}>{student.firstName} {student.lastName}</div>))
+        :
+        <div>Sorry, but no students could be found</div>
+      }
     </div>
   );
 }
