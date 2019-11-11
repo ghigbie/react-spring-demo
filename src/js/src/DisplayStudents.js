7fb27b5126825d92c5949d785694befcc93787de
ghigbie <gehigbie@gmail.com>
2019-11-10 19:51:59 -0700

    Install antd

--- src/js/src/DisplayStudents.js
+++ src/js/src/DisplayStudents.js
@@ -1,5 +1,8 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { getAllStudents } from './client';
+import { 
+    Table
+} from 'antd';
 
 const DisplayStudents = () => {
     const [students, setStudents] = useState([]);
@@ -18,9 +21,14 @@ const DisplayStudents = () => {
 
     return (
         <Fragment>
-            {students ?
+            {students && students.length ?
                 students.map(student => (
-                    <div key={student.studentId}>{student.firstName} {student.lastName}
+                    <div key={student.studentId}>
+                        <h2> {student.studentId }</h2>
+                        <p>{ student.firstName }</p>
+                        <p>{ student.lastName }</p>
+                        <p>{ student.gender }</p>
+                        <p>{ student.email }</p>
                     </div>))
                 :
                 <div>Sorry, but no students could be found</div>
